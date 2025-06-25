1ff4b46ec871c05af0ca090f7ae9f1e8a577f8df
Samarthgit-code <samarth192025@gmail.com>
2025-06-25 15:44:57 +0530

    Update settings.py

--- Video_Platform/settings.py
+++ Video_Platform/settings.py
@@ -95,6 +95,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 # ========================
 # ✅ Media (only if needed)
 # ========================
