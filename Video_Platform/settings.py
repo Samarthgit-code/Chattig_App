8915509d09fc6008f6761da7b64adcc424249724
Samarthgit-code <samarth192025@gmail.com>
2025-06-25 15:32:47 +0530

    Update settings.py

--- Video_Platform/settings.py
+++ Video_Platform/settings.py
@@ -22,9 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app',
-    'cloudinary',
-    'cloudinary_storage',
+    'app'
 ]
 
 MIDDLEWARE = [
